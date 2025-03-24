2b66c36d76e1b293db33762479d95fd629cfb743
OmmDevgoswami <teamuniverse.omm@gmail.com>
2025-03-24 18:31:55 +0530

    Examples Added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import './cardstyle.css'
-
+import './cardstyle.css';
 
 const Card = (props) => {
-    
-    const { sourceUrl, meaningofWord, word, phonetic, antonyms, synonyms  } = props.info;
+    const { sourceUrl, meaningofWord, word, phonetic, antonyms, synonyms } = props.info;
     const audioUrl = props.info.audio;
-
-
 
     return (
         <div className='cardContainer'>
@@ -15,28 +11,25 @@ const Card = (props) => {
             <p>{phonetic}</p>
 
             <p>Pronounce: </p>
-            
             <audio key={Math.random()} controls>
-                <source src={audioUrl} type="audio/ogg">
-                </source></audio>
-                
-            {
-                meaningofWord !== undefined ? meaningofWord.map((val, index) => {
-                    return <div key={index}>
-                        <h2 className='partofSpeech'>{val.partOfSpeech ? (val.partOfSpeech).toUpperCase() : "Wait..."} :-</h2>
+                <source src={audioUrl} type="audio/ogg" />
+            </audio>
+
+            {meaningofWord !== undefined ? meaningofWord.map((val, index) => {
+                return (
+                    <div key={index}>
+                        <h2 className='partofSpeech'>{val.partOfSpeech ? val.partOfSpeech.toUpperCase() : "Wait..."} :-</h2>
                         <ul>
-                            {
-                                val.definitions.map((def) => {
-                                    return <li key={Math.random()}>{def.definition}</li>
-                                })
-                            }
+                            {val.definitions.map((def, defIndex) => (
+                                <li key={defIndex}>{def.definition}</li>
+                            ))}
                         </ul>
-
                     </div>
-                }) : console.log("Fetching Meaning...")
-            }
+                );
+            }) : console.log("Fetching Meaning...")}
 
-        <div>
+            {/* Antonyms Section */}
+            <div>
                 <h2 className='partofSpeech'> ANTONYMS:- </h2>
                 {antonyms && antonyms.length > 0 ? (
                     <ul>
@@ -49,6 +42,7 @@ const Card = (props) => {
                 )}
             </div>
 
+            {/* Synonyms Section */}
             <div>
                 <h2 className='partofSpeech'> SYNONYMS :- </h2>
                 {synonyms && synonyms.length > 0 ? (
@@ -60,18 +54,34 @@ const Card = (props) => {
                 ) : (
                     <p>No synonyms available</p>
                 )}
-            </div>      
-
-            <div className='linkContainer'>
-                <a href={sourceUrl}>More Info</a>
-                <br></br><br></br>
-                <a href="https://about.me/subhranshu">@Subhranhsu {new Date().getFullYear()}</a>
             </div>
 
+            {/* Examples / Uses Section */}
+            <div>
+                <h2 className='partofSpeech'> EXAMPLES / USES :- </h2>
+                {meaningofWord && meaningofWord.some(m => m.definitions.some(d => d.example)) ? (
+                    meaningofWord.map((val, index) => (
+                        <div key={index}>
+                            <ul>
+                            {val.definitions.map((def, defIndex) => (
+                                def.example ? <li key={defIndex}> {def.example}</li> : null
+                            ))}
+                            </ul>
+                        </div>
+                    ))
+                ) : (
+                    <p>No examples available</p>
+                )}
+            </div>
 
+            {/* More Info */}
+            <div className='linkContainer'>
+                <a href={sourceUrl}>More Info</a>
+                <br /><br />
+                <a href="https://about.me/subhranshu">@Subhranhsu {new Date().getFullYear()}</a>
+            </div>
         </div>
     );
 }
 
 export default Card;
-
